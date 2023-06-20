wejago/mqtt2influx

Store data in Kafka instead of influxdb

src/main/java/de/wejago/mqtt2influx/service/RawSubscriberService.java
@@ -1,60 +1,79 @@
 package de.wejago.mqtt2influx.service;
 
-import com.influxdb.client.domain.WritePrecision;
-import com.influxdb.client.write.Point;
 import de.wejago.mqtt2influx.config.Device;
-import de.wejago.mqtt2influx.repository.InfluxDbRepository;
+import de.wejago.mqtt2influx.config.MqttDataPoint;
+import de.wejago.mqtt2influx.repository.KafkaRepository;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 @Component
 @RequiredArgsConstructor
 @EnableScheduling
 @Getter
 public class RawSubscriberService {
-    private final InfluxDbRepository influxDbRepository;
+    private final KafkaRepository kafkaRepository;
 
     // This map stores the points for each device to be written to the database
     // Key: Sensor ID of the device
-    private final Map<String, Point> points = new ConcurrentHashMap<>();
+    private final Map<String, MqttDataPoint> points = new ConcurrentHashMap<>();
     private static final int SCHEDULE_INTERVAL_PERSIST_TO_QUEUE = 60000;
 
     public void updatePoint(Device device, String key, Double measurement) {
         if (!points.containsKey(device.getSensorId())) {
-            Point point = createPoint(device);
-            points.put(device.getSensorId(), point);
+            MqttDataPoint mqttDataPoint = createMqttDataPoint(device);
+            points.put(device.getSensorId(), mqttDataPoint);
         }
-
-        points.get(device.getSensorId()).addField(device.getMappings().get(key), measurement);
+        this.updateMqttDataFields(device, key, measurement);
     }
 
     @Scheduled(fixedRate = SCHEDULE_INTERVAL_PERSIST_TO_QUEUE)
     public void scheduleAddPointToQueue() {
-        Iterator<Map.Entry<String, Point>> iterator = points.entrySet().iterator();
+        Iterator<Map.Entry<String, MqttDataPoint>> iterator = points.entrySet().iterator();
 
         while (iterator.hasNext()) {
-            Map.Entry<String, Point> entry = iterator.next();
-            Point point = entry.getValue();
+            Map.Entry<String, MqttDataPoint> entry = iterator.next();
+            MqttDataPoint point = entry.getValue();
 
-            if (point.hasFields()) {
-                point.time(Instant.now(), WritePrecision.MS);
-                influxDbRepository.writePoint(point);
+            if (point.getFields() != null
+                && point.getFields().size() > 0) {
+                point.setTime(Instant.now());
+                kafkaRepository.writePoint(point);
+                System.out.println("raw subscriber: " + point.getDevice_name());
                 iterator.remove();
             }
         }
     }
 
-    private Point createPoint(Device device) {
-        return Point.measurement("sensor")
-                    .addTag("device_name", device.getName())
-                    .addTag("sensor_id", device.getSensorId());
+    private void updateMqttDataFields(Device device, String key, Double measurement) {
+        String pointKey = key;
+        if(device.getMappings().containsKey(key)){
+            pointKey = device.getMappings().get(key);
+        }
+
+        if (points.get(device.getSensorId()).getFields() != null
+            && points.get(device.getSensorId()).getFields().size() > 0
+            && points.get(device.getSensorId()).getFields().containsKey(key)) {
+            // update existing measurement
+            points.get(device.getSensorId()).getFields().replace(pointKey, measurement);
+        } else {
+            // add new measurement to the point
+            points.get(device.getSensorId()).getFields().put(pointKey, measurement);
+        }
+    }
+
+    private MqttDataPoint createMqttDataPoint(Device device) {
+        MqttDataPoint mqttDataPoint = new MqttDataPoint();
+        mqttDataPoint.setDevice_name(device.getName());
+        mqttDataPoint.setSensor_id(device.getSensorId());
+        mqttDataPoint.setFields(new HashMap<>());
+        return mqttDataPoint;
     }
 }

src/test/java/de/wejago/mqtt2influx/service/JsonSubscriberTest.java
@@ -10,7 +10,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.influxdb.client.domain.WritePrecision;
 import com.influxdb.client.write.Point;
 import de.wejago.mqtt2influx.config.Device;
+import de.wejago.mqtt2influx.config.MqttDataPoint;
 import de.wejago.mqtt2influx.repository.InfluxDbRepository;
+import de.wejago.mqtt2influx.repository.KafkaRepository;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,7 +28,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class JsonSubscriberTest {
     private ObjectMapper objectMapper = new ObjectMapper();
     @Mock
-    private InfluxDbRepository influxDbRepository;
+    private KafkaRepository kafkaRepository;
 
     private Device device;
     private JsonSubscriber jsonSubscriber;
@@ -34,7 +36,7 @@ class JsonSubscriberTest {
     @BeforeEach
     public void setUp() {
         buildTestDevice();
-        jsonSubscriber = new JsonSubscriber(objectMapper, influxDbRepository, device);
+        jsonSubscriber = new JsonSubscriber(objectMapper, kafkaRepository, device);
     }
 
     @Test
@@ -43,14 +45,14 @@ class JsonSubscriberTest {
         String receivedMessage = "{\"Time\":\"2023-05-10T10:16:32\",\"\":{\"Total_in\":695.38,\"Total_out\":16.94,\"Power_curr\":1151," +
                                  "\"device_id\":\"0a01454d480000b22b25\"}}";
         MqttMessage mqttMessage = new MqttMessage(receivedMessage.getBytes());
-        Point generatedTestPoint = buildTestPoint();
+        MqttDataPoint generatedTestPoint = buildTestMqttPoint();
 
         // WHEN
         jsonSubscriber.messageArrived("testTopic", mqttMessage);
 
         // THEN
-        ArgumentCaptor<Point> captor = ArgumentCaptor.forClass(Point.class);
-        verify(influxDbRepository, times(1)).writePoint(captor.capture());
+        ArgumentCaptor<MqttDataPoint> captor = ArgumentCaptor.forClass(MqttDataPoint.class);
+        verify(kafkaRepository, times(1)).writePoint(captor.capture());
         assertThat(captor.getValue())
                 .usingRecursiveComparison()
                 .ignoringFields("time")
@@ -67,7 +69,7 @@ class JsonSubscriberTest {
         jsonSubscriber.messageArrived("testTopic", mqttMessage);
 
         // THEN
-        verify(influxDbRepository, never()).writePoint(any());
+        verify(kafkaRepository, never()).writePoint(any());
     }
 
     @Test
@@ -79,7 +81,7 @@ class JsonSubscriberTest {
         jsonSubscriber.messageArrived("testTopic", mqttMessage);
 
         // THEN
-        verify(influxDbRepository, never()).writePoint(any());
+        verify(kafkaRepository, never()).writePoint(any());
     }
 
     private void buildTestDevice() {
@@ -95,13 +97,16 @@ class JsonSubscriberTest {
         device.setMappings(deviceMappings);
     }
 
-    private Point buildTestPoint() {
-        return Point.measurement("sensor")
-                .time(Instant.now(), WritePrecision.MS)
-                .addTag("sensor_id", "0a01454d480000b22b25")
-                .addTag("device_name", "testName")
-                .addField("Total Consumption", 695.38)
-                .addField("Total Production", 16.94)
-                .addField("Current Consumption", 1151D);
+    private MqttDataPoint buildTestMqttPoint() {
+        MqttDataPoint testMqttPoint = new MqttDataPoint();
+        testMqttPoint.setSensor_id("0a01454d480000b22b25");
+        testMqttPoint.setDevice_name("testName");
+        testMqttPoint.setTime(Instant.now());
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("Total Consumption", 695.38);
+        fields.put("Total Production", 16.94);
+        fields.put("Current Consumption", 1151D);
+        testMqttPoint.setFields(fields);
+        return testMqttPoint;
     }
 }

emiliyank/mqtt2kafka

src/main/java/de/wejago/mqtt2influx/service/RawSubscriberService.java
@@ -1,60 +1,78 @@
 package de.wejago.mqtt2influx.service;
 
-import com.influxdb.client.domain.WritePrecision;
-import com.influxdb.client.write.Point;
 import de.wejago.mqtt2influx.config.Device;
-import de.wejago.mqtt2influx.repository.InfluxDbRepository;
+import de.wejago.mqtt2influx.config.MqttDataPoint;
+import de.wejago.mqtt2influx.repository.KafkaProducer;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.Instant;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 @Component
 @RequiredArgsConstructor
 @EnableScheduling
 @Getter
 public class RawSubscriberService {
-    private final InfluxDbRepository influxDbRepository;
+    private final KafkaProducer kafkaProducer;
 
     // This map stores the points for each device to be written to the database
     // Key: Sensor ID of the device
-    private final Map<String, Point> points = new ConcurrentHashMap<>();
+    private final Map<String, MqttDataPoint> points = new ConcurrentHashMap<>();
     private static final int SCHEDULE_INTERVAL_PERSIST_TO_QUEUE = 60000;
 
     public void updatePoint(Device device, String key, Double measurement) {
         if (!points.containsKey(device.getSensorId())) {
-            Point point = createPoint(device);
-            points.put(device.getSensorId(), point);
+            MqttDataPoint mqttDataPoint = createMqttDataPoint(device);
+            points.put(device.getSensorId(), mqttDataPoint);
         }
-
-        points.get(device.getSensorId()).addField(device.getMappings().get(key), measurement);
+        this.updateMqttDataFields(device, key, measurement);
     }
 
     @Scheduled(fixedRate = SCHEDULE_INTERVAL_PERSIST_TO_QUEUE)
     public void scheduleAddPointToQueue() {
-        Iterator<Map.Entry<String, Point>> iterator = points.entrySet().iterator();
+        Iterator<Map.Entry<String, MqttDataPoint>> iterator = points.entrySet().iterator();
 
         while (iterator.hasNext()) {
-            Map.Entry<String, Point> entry = iterator.next();
-            Point point = entry.getValue();
+            Map.Entry<String, MqttDataPoint> entry = iterator.next();
+            MqttDataPoint point = entry.getValue();
 
-            if (point.hasFields()) {
-                point.time(Instant.now(), WritePrecision.MS);
-                influxDbRepository.writePoint(point);
+            if (point.getFields() != null
+                && point.getFields().size() > 0) {
+                point.setTime(Instant.now());
+                kafkaProducer.writePoint(point);
                 iterator.remove();
             }
         }
     }
 
-    private Point createPoint(Device device) {
-        return Point.measurement("sensor")
-                    .addTag("device_name", device.getName())
-                    .addTag("sensor_id", device.getSensorId());
+    private void updateMqttDataFields(Device device, String key, Double measurement) {
+        String pointKey = key;
+        if(device.getMappings().containsKey(key)){
+            pointKey = device.getMappings().get(key);
+        }
+
+        if (points.get(device.getSensorId()).getFields() != null
+            && points.get(device.getSensorId()).getFields().size() > 0
+            && points.get(device.getSensorId()).getFields().containsKey(key)) {
+            // update existing measurement
+            points.get(device.getSensorId()).getFields().replace(pointKey, measurement);
+        } else {
+            // add new measurement to the point
+            points.get(device.getSensorId()).getFields().put(pointKey, measurement);
+        }
+    }
+
+    private MqttDataPoint createMqttDataPoint(Device device) {
+        MqttDataPoint mqttDataPoint = new MqttDataPoint();
+        mqttDataPoint.setDevice_name(device.getName());
+        mqttDataPoint.setSensor_id(device.getSensorId());
+        mqttDataPoint.setFields(new HashMap<>());
+        return mqttDataPoint;
     }
 }

src/test/java/de/wejago/mqtt2influx/service/JsonSubscriberTest.java
@@ -7,10 +7,9 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.influxdb.client.domain.WritePrecision;
-import com.influxdb.client.write.Point;
 import de.wejago.mqtt2influx.config.Device;
-import de.wejago.mqtt2influx.repository.InfluxDbRepository;
+import de.wejago.mqtt2influx.config.MqttDataPoint;
+import de.wejago.mqtt2influx.repository.KafkaProducer;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,7 +25,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class JsonSubscriberTest {
     private ObjectMapper objectMapper = new ObjectMapper();
     @Mock
-    private InfluxDbRepository influxDbRepository;
+    private KafkaProducer kafkaProducer;
 
     private Device device;
     private JsonSubscriber jsonSubscriber;
@@ -34,7 +33,7 @@ class JsonSubscriberTest {
     @BeforeEach
     public void setUp() {
         buildTestDevice();
-        jsonSubscriber = new JsonSubscriber(objectMapper, influxDbRepository, device);
+        jsonSubscriber = new JsonSubscriber(objectMapper, kafkaProducer, device);
     }
 
     @Test
@@ -43,14 +42,14 @@ class JsonSubscriberTest {
         String receivedMessage = "{\"Time\":\"2023-05-10T10:16:32\",\"\":{\"Total_in\":695.38,\"Total_out\":16.94,\"Power_curr\":1151," +
                                  "\"device_id\":\"0a01454d480000b22b25\"}}";
         MqttMessage mqttMessage = new MqttMessage(receivedMessage.getBytes());
-        Point generatedTestPoint = buildTestPoint();
+        MqttDataPoint generatedTestPoint = buildTestMqttPoint();
 
         // WHEN
         jsonSubscriber.messageArrived("testTopic", mqttMessage);
 
         // THEN
-        ArgumentCaptor<Point> captor = ArgumentCaptor.forClass(Point.class);
-        verify(influxDbRepository, times(1)).writePoint(captor.capture());
+        ArgumentCaptor<MqttDataPoint> captor = ArgumentCaptor.forClass(MqttDataPoint.class);
+        verify(kafkaProducer, times(1)).writePoint(captor.capture());
         assertThat(captor.getValue())
                 .usingRecursiveComparison()
                 .ignoringFields("time")
@@ -67,7 +66,7 @@ class JsonSubscriberTest {
         jsonSubscriber.messageArrived("testTopic", mqttMessage);
 
         // THEN
-        verify(influxDbRepository, never()).writePoint(any());
+        verify(kafkaProducer, never()).writePoint(any());
     }
 
     @Test
@@ -79,7 +78,7 @@ class JsonSubscriberTest {
         jsonSubscriber.messageArrived("testTopic", mqttMessage);
 
         // THEN
-        verify(influxDbRepository, never()).writePoint(any());
+        verify(kafkaProducer, never()).writePoint(any());
     }
 
     private void buildTestDevice() {
@@ -95,13 +94,16 @@ class JsonSubscriberTest {
         device.setMappings(deviceMappings);
     }
 
-    private Point buildTestPoint() {
-        return Point.measurement("sensor")
-                .time(Instant.now(), WritePrecision.MS)
-                .addTag("sensor_id", "0a01454d480000b22b25")
-                .addTag("device_name", "testName")
-                .addField("Total Consumption", 695.38)
-                .addField("Total Production", 16.94)
-                .addField("Current Consumption", 1151D);
+    private MqttDataPoint buildTestMqttPoint() {
+        MqttDataPoint testMqttPoint = new MqttDataPoint();
+        testMqttPoint.setSensor_id("0a01454d480000b22b25");
+        testMqttPoint.setDevice_name("testName");
+        testMqttPoint.setTime(Instant.now());
+        Map<String, Object> fields = new HashMap<>();
+        fields.put("Total Consumption", 695.38);
+        fields.put("Total Production", 16.94);
+        fields.put("Current Consumption", 1151D);
+        testMqttPoint.setFields(fields);
+        return testMqttPoint;
     }
 }